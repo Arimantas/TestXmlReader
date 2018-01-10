668c5c1ae35fc11bd1b8c1f76a2a0c970616466a
Martynas <packmaster@riawolf.com>
2018-01-10 17:59:32 +0200

    pull request

--- Bandomoji/AnimalFactory.cs
+++ Bandomoji/AnimalFactory.cs
@@ -5,43 +5,34 @@ namespace Bandomoji
 {
     class AnimalFactory
     {
-        private string _localClass { get; set; }
-        private string _localName { get; set; }
-        private double _localWeight { get; set; }
-        private double _localHeight { get; set; }
-        private string _localMove { get; set; }
+        
 
         // nors list ir dinaminis, cia viskas statiska.. kolkas
-        public Animal MakeAnimal(string newAnimalType)
+        public List<Animal> MakeAnimal(string newAnimalType)
         {
-            if (newAnimalType.Equals("1"))
-            {
-                ReadXmlFile("Dog"); // pasirenku kuri parametra nuskaitineti
-                return new Dog(_localClass, _localName, _localWeight, _localHeight, _localMove);
-            }
-            else if (newAnimalType.Equals("2"))
-            {
-                ReadXmlFile("Cat");
-                return new Cat(_localClass, _localName, _localWeight, _localHeight, _localMove);
-            }
-            else if (newAnimalType.Equals("3"))
-            {
-                ReadXmlFile("Pigeon");
-                return new Pigeon(_localClass, _localName, _localWeight, _localHeight, _localMove);
-            }
-            else
-            {
-                Console.WriteLine("Tokio pasirinkimo nera");
-                return null;
-            } 
+			List<Animal> result = new List<Animal>();
+			XmlDocument doc = new XmlDocument();
+            doc.Load("F:\\KCS\\Bandomoji uzduotis\\Bandomoji\\Bandomoji\\animals.xml");
+
+            XmlNodeList animalsNodes = doc.SelectSingleNode("Animals").ChildNodes;
+			foreach(XmlNode animalNode in animalsNodes){
+				switch (animalNode.Name)
+				{
+					case "Dog":
+						result = new Dog();
+						
+					break;
+					
+				}
+				if(result != null){
+					
+				}
+			}
         }
 
         private void ReadXmlFile(string elementType)
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load("F:\\KCS\\Bandomoji uzduotis\\Bandomoji\\Bandomoji\\animals.xml");
-
-            XmlNodeList animals = doc.GetElementsByTagName(elementType);
+            
             foreach (XmlNode animalNode in animals)
             {
                 _localClass = animalNode.SelectSingleNode("AnimalClass").InnerText;
